2f57e1a7fb2f6b9efaefe1c9cc1c29524bbda5ac
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-10-28 18:31:04 +0900

    Update CHROMOSOME.cpp

--- Homework/CHROMOSOME.cpp
+++ Homework/CHROMOSOME.cpp
@@ -3,27 +3,28 @@
 #include <fstream>
 #include <vector>
 using namespace std;
-void fr(int s, int e, list <char>& chro) {
+
+void my_front(int s, int e, list <char>& chro) {
    if (e>chro.size())return;
    auto s_it = chro.begin(), e_it = chro.begin(); advance(s_it, s - 1); advance(e_it, e);
    chro.splice(chro.begin(),chro,s_it,e_it);
 }
-void ba(int s, int e, list <char>& chro) {
+void my_back(int s, int e, list <char>& chro) {
    if (e>chro.size())return;
    auto s_it = chro.begin(), e_it = chro.begin(); advance(s_it, s - 1); advance(e_it, e);
    chro.splice(chro.end(),chro,s_it,e_it);
 }
-void cu(int s, int e, list <char>& chro) {
+void cut(int s, int e, list <char>& chro) {
    if (e>chro.size())return;
    auto s_it = chro.begin(), e_it = chro.begin(); advance(s_it, s - 1); advance(e_it, e);
    chro.erase(s_it,e_it);
 }
-void dou(int s, int e, list <char>& chro) {
+void my_double(int s, int e, list <char>& chro) {
    if (e>chro.size())return;
    auto s_it = chro.begin(), e_it = chro.begin(); advance(s_it, s - 1); advance(e_it, e);
    chro.insert(e_it,s_it,e_it);
 }
-void fl(int s, int e, list <char>& chro) {
+void my_flip(int s, int e, list <char>& chro) {
    if (e>chro.size())return;
    list<char> temp;
    auto s_it = chro.begin(), e_it = chro.begin(); advance(s_it,s-1); advance(e_it, e);
@@ -31,7 +32,7 @@ void fl(int s, int e, list <char>& chro) {
    s_it = chro.begin(); advance(s_it,s-1);
    chro.splice(s_it,temp,temp.begin(),temp.end());
 }
-void rep(int s, int e, list <char> chro) {
+void my_report(int s, int e, list <char> chro) {
    if (e>chro.size()) e = chro.size();
    if (s>chro.size()) {
       cout <<"NONE"<< endl; return;
@@ -55,12 +56,12 @@ void act_in(int n, list<char> &chro) {
       cin >> s >> e;
       int code = act(action);
       switch(code){
-      case 0: fr(s,e,chro); break;
-      case 1: ba(s,e,chro); break;
-      case 2: cu(s,e,chro); break;
-      case 3: dou(s,e,chro); break;
-      case 4: fl(s,e,chro); break;
-      case 5: rep(s,e,chro); break;
+      case 0: my_front(s,e,chro); break;
+      case 1: my_back(s,e,chro); break;
+      case 2: my_cut(s,e,chro); break;
+      case 3: my_double(s,e,chro); break;
+      case 4: my_flip(s,e,chro); break;
+      case 5: my_report(s,e,chro); break;
       }
    }
 }
