2f2a4726f24fdecc46ac6d306607f69027103daa
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-12-18 00:13:12 +0900

    Update and rename HW17_CPE(미완).cpp to HW17_CPE(80%).cpp

--- Homework/HW17_CPE(80%).cpp
+++ Homework/HW17_CPE(80%).cpp
@@ -5,30 +5,35 @@ using namespace std;
 
 vector<string> s_group;
 vector<set<string>> substrings;
-bool flag = true;
 void store_sentece(int n) {
+	string sentence;
 	for (int i = 0; i < n; i++) {
-		char sentence = '0';
 		string temp;
 		while (true) {
 			cin >> sentence;
-			if (sentence == '$') break;
-			if ((sentence & 0x80) != 0)	temp.push_back(sentence);
+			if (sentence.compare("$") == 0) break;
+			int nn = sentence.size();
+			for (int i = 0; i < nn; i++) {
+				if ((sentence[i] & 0x80) != 0)	temp.push_back(sentence[i]);
+			}
 		}
-		if (temp.size() != 0) s_group.push_back(temp);
+		s_group.push_back(temp);
 	}
-	if (s_group.size() <= 1) flag = false;
 }
 void make_substrings() {
-	if (flag == false) return;
 	for (int i = 0; i < s_group.size(); i++) {
 		set<string> temp;
-		for (int j = 0; j < s_group[i].size()-2; j += 2) temp.insert(s_group[i].substr(j, 4));
+		if (s_group[i].size() == 0) { temp.insert(s_group[i]); }
+		else {
+			for (int j = 0; j < s_group[i].size() - 2; j += 2) {
+				temp.insert(s_group[i].substr(j, 4));
+			}
+		}
 		substrings.push_back(temp);
 	}
 }
-void compare_plagiarism(pair<int, int> &plagi_idx) {
-	if (flag == false) return;
+void compare_plagiarism() {
+	pair<int, int> plagi_idx;
 	double length_a = 0, length_b = 0, fin_length = 0;
 	double plagi_ratio = 0, max_ratio = 0;
 	for (int i = 0; i < substrings.size(); i++) {
@@ -36,6 +41,7 @@ void compare_plagiarism(pair<int, int> &plagi_idx) {
 		length_a = substrings[i].size();
 		for (int j = i+1; j < substrings.size(); j++) {
 			length_b = substrings[j].size();
+			if (length_a == 1 || length_b == 1) continue;
 			temp.insert(substrings[i].begin(), substrings[i].end());
 			temp.insert(substrings[j].begin(), substrings[j].end());
 			fin_length = temp.size();
@@ -47,14 +53,13 @@ void compare_plagiarism(pair<int, int> &plagi_idx) {
 			temp.clear();
 		}
 	}
+	cout << plagi_idx.first << " " << plagi_idx.second;
 }
 int main() {
 	int n;
-	pair<int, int> plagi_idx;
 	cin >> n;
 	store_sentece(n);
 	make_substrings();
-	compare_plagiarism(plagi_idx);
-	cout << plagi_idx.first << " " << plagi_idx.second;
+	compare_plagiarism();
 	return 0;
 }
