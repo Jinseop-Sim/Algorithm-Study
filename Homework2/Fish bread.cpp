5b62b01fba7e13905e5ea9518ef2675070925498
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2022-03-12 00:36:25 +0900

    Update Fish bread.cpp

--- Homework2/Fish bread.cpp
+++ Homework2/Fish bread.cpp
@@ -1,50 +1,44 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <stack>
 using namespace std;
 
-void make_loaf(vector<int> breads, int& cnt) {
+bool is_origin(vector<int> breads) {
 	for (int i = 0; i < breads.size(); i++) {
-		if (i == 0) {
-			if (abs(breads[i]) != i + 1) {
-				if (abs(breads[i]) - abs(breads[i + 1]) != 1) cnt += 1;
-				else if (breads[i] * breads[i + 1] < 0) cnt += 1;
-			}
-		}
-		else {
-			if (abs(breads[i]) != i + 1) {
-				if (abs(breads[i - 1]) - abs(breads[i]) != 1) cnt += 1;
-				else if (breads[i] * breads[i - 1] < 0) cnt += 1;
-			}
-		}
+		if (breads[i] != i + 1) return false;
 	}
 }
 
-void count_unary(vector<int> breads, int& cnt) {
-	if (breads[0] != 1) cnt += 1;
+bool find_reverse(vector<int>& breads) {
+	int org_value = 0, org_idx = 0;
 	for (int i = 0; i < breads.size(); i++) {
-		if (breads[i] < 0) {
-			if (i == breads.size() - 1) {
-				if (abs(breads[i]) - abs(breads[i - 1]) == 1) cnt += 1; continue;
+		if (breads[i] != i + 1) {
+			org_value = i + 1;
+			for (int j = i; j < breads.size(); j++) {
+				if (abs(breads[j]) == org_value) { org_idx = j; break; }
 			}
-			if (abs(breads[i + 1]) - abs(breads[i]) == 1) cnt += 1;
+			reverse(breads.begin() + i, breads.begin()+org_idx+1);
+			for (int j = i; j < org_idx + 1; j++) { breads[j] = breads[j] * (-1); }
+			break;
 		}
 	}
+	return is_origin(breads);
 }
 
-void say_answer(int cnt) {
-	switch (cnt) {
-	case 1:
-		cout << "one" << endl;
-		break;
-	case 2:
-		cout << "two" << endl;
-		break;
-	default:
-		cout << "over" << endl;
-		break;
+bool back_find_reverse(vector<int>& breads) {
+	int org_value = 0, org_idx = 0;
+	for (int i = breads.size()-1; i >= 0; i--) {
+		if (breads[i] != i + 1) {
+			org_value = i + 1;
+			for (int j = i; j >= 0; j--) {
+				if (abs(breads[j]) == org_value) { org_idx = j; break; }
+			}
+			reverse(breads.begin() + org_idx, breads.begin() + i + 1);
+			for (int j = org_idx; j < i + 1; j++) { breads[j] = breads[j] * (-1); }
+			break;
+		}
 	}
+	return is_origin(breads);
 }
 
 int main() {
@@ -53,18 +47,22 @@ int main() {
 
 	for (int i = 0; i < 5; i++) {
 		vector<int> breads;
+		vector<int> rev_breads;
+		bool is_once = true;
 		int bread, cnt = 0;
 
 		for (int j = 0; j < n; j++) {
 			cin >> bread;
 			breads.push_back(bread);
+			rev_breads.push_back(bread);
 		}
 
-		make_loaf(breads, cnt);
-		count_unary(breads, cnt);
-
-		say_answer(cnt);
+		if (find_reverse(breads)) cout << "one" << endl;
+		else {
+			back_find_reverse(rev_breads);
+			if (find_reverse(breads) || back_find_reverse(rev_breads)) { cout << "two" << endl; }
+			else cout << "over" << endl;
+		}
 	}
-
 	return 0;
 }
