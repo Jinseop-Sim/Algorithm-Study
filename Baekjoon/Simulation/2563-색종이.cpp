6da11817353daa55c4fb12509d2bf2b9d7a67537
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2022-03-26 14:22:20 +0900

    Update 2563-색종이.cpp

--- Baekjoon/Simulation/2563-색종이.cpp
+++ Baekjoon/Simulation/2563-색종이.cpp
@@ -4,34 +4,24 @@ using namespace std;
 
 vector<vector<int>> paper(100, vector<int>(100));
 
-void make_map() {
+int make_map() {
 	int n = 0, x = 0, y = 0;
+	int area = 0;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> x >> y;
 		for (int row = x; row < x + 10; row++) {
 			for (int col = y; col < y + 10; col++) {
 				if (paper[row][col] == 1) continue;
-				paper[row][col] = 1;
+				paper[row][col] = 1; area += 1;
 			}
-		}
-	}
-}
-
-int count_area() {
-	int area = 0;
-	for (int i = 0; i < 100; i++) {
-		for (int j = 0; j < 100; j++) {
-			if (paper[i][j] == 1) area += 1;
 		}
 	}
 
 	return area;
 }
+
 int main() {
-	make_map();
-
-	cout << count_area();
-
+	cout << make_map();
 	return 0;
 }
