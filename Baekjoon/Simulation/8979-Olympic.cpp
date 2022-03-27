36b21e86e6816ff596d84b3e295421a75c4c9534
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2022-03-27 19:21:00 +0900

    Update 8979-Olympic.cpp

--- Baekjoon/Simulation/8979-Olympic.cpp
+++ Baekjoon/Simulation/8979-Olympic.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <vector>
 using namespace std;
 
 map<int, pair<int, int>> rank_info;
+vector<int> names;
 void get_input(int n) {
 	for (int i = 0; i < n; i++) {
-		int c;
-		string g, s, b, info;
+		int c, g, s, b, score;
 		cin >> c >> g >> s >> b;
-		info = g + s + b;
-		rank_info[c] = make_pair(stoi(info), 1);
+		names.push_back(c);
+		score = g * 100000 + s * 100 + b;
+		rank_info[c] = make_pair(score, 1);
 	}
 }
 
 void make_rank(int n) {
-	for (int i = 1; i < n + 1; i++) {
-		for (int j = 1; j < n + 1; j++) {
-			if (rank_info[i].first < rank_info[j].first) rank_info[i].second++;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (rank_info[names[i]].first < rank_info[names[j]].first) rank_info[names[i]].second++;
 		}
 	}
 }
