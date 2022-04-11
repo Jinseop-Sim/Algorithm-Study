98b54b45f0c6a330c4d95aa221b28335fc1790ef
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2022-04-11 11:09:56 +0900

    Create 11866-요세푸스 문제.cpp

---
+++ Baekjoon/Structure/11866-요세푸스 문제.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+queue<int> kill_queue;
+
+void init_queue(int n) {
+	for (int i = 0; i < n; i++) kill_queue.push(i + 1);
+}
+
+void rot_left() {
+	kill_queue.push(kill_queue.front());
+	kill_queue.pop();
+}
+
+void josephus(int k) {
+	while (!kill_queue.empty()) {
+		for (int i = 0; i < k - 1; i++) rot_left();
+		if (kill_queue.size() == 1) cout << kill_queue.front();
+		else cout << kill_queue.front() << ", ";
+		kill_queue.pop();
+	}
+}
+int main() {
+	int n = 0, k = 0;
+	cin >> n >> k;
+
+	init_queue(n);
+	
+	cout << "<";
+	josephus(k);
+	cout << ">";
+	return 0;
+}
