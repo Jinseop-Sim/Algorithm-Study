5b98ff0523d163e4d15ed0f97426faf4dd1d84c7
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2022-05-19 21:47:32 +0900

    Update 9663-NQueen.cpp

--- Baekjoon/Graph/9663-NQueen.cpp
+++ Baekjoon/Graph/9663-NQueen.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int n = 0, cnt = 0;
-bool check(int depth, vector<int>& visit) {
+bool check(int depth, vector<int>& visit) { // 놓을 수 있는 위치인지 검사한다.
 	for (int i = 0; i < depth; i++)
-		if (visit[i] == visit[depth] || abs(visit[depth] - visit[i]) == depth - i)
-			return false;
+		if (visit[i] == visit[depth] || abs(visit[depth] - visit[i]) == depth - i) // 먼저 같은 선상에 있는지 앞에서 검사하고
+			return false;                          // x값의 차이와 y값이 차이가 같으면 대각선에 위치해있다는 말!
 	return true;
 }
 
